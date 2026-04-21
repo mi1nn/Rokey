c4ed2bc0628a27119522bd057e78eb809e0a7853
mi1nn <minbossf1@gmail.com>
2026-04-21 16:26:16 +0900

    ch08

--- ch08/find_max1.py
+++ ch08/find_max1.py
@@ -8,11 +8,11 @@ def fmax(a, b, c, d, e):
 
     if max < b:
         max = b
-    elif max < c:
+    if max < c:    ## elif로 작성할 경우, 해당 조건이 참이면 뒤를 검사하지 않는다. (처음 True에서 종료)
         max = c
-    elif max < d:
+    if max < d:
         max = d
-    elif max < e:
+    if max < e:
         max = e
     
     return max
@@ -35,4 +35,93 @@ def fmax2(a, b, c, d, e):
     return max
 
 max = fmax2(u[0], u[1], u[2], u[3], u[4])
+print("최대값: ", max)
+
+print()
+print('------------------------------')
+
+def fmax3(a, b, c, d, e):
+    
+    fu = [a, b, c, d, e]
+    max = fu[0]    
+
+    for i in fu:
+        if max < i:
+            max = i
+    
+    return max
+
+max = fmax3(u[0], u[1], u[2], u[3], u[4])
+print("최대값: ", max)
+
+print()
+print('------------------------------')
+
+def fmax4(a, b, c, d, e):
+    
+    fu = []
+    fu.append(a)
+    fu.append(b)
+    fu.append(c)
+    fu.append(d)
+    fu.append(e)
+    
+    max = fu[0]
+
+    for i in fu:
+        if max < i:
+            max = i
+    
+    return max
+
+max = fmax4(u[0], u[1], u[2], u[3], u[4])
+print("최대값: ", max)
+
+print()
+print('------------------------------')
+
+def fmax5(fu):
+    
+    max = fu[0]    
+
+    for i in fu:
+        if max < i:
+            max = i
+    
+    return max
+
+max = fmax5(u)
+print("최대값: ", max)
+
+print()
+print('------------------------------')
+
+def fmax6(fu):
+    
+    max = fu[0]    
+
+    for i in range(1, 5, 1):
+        if max < fu[i]:
+            max = fu[i]
+    
+    return max
+
+max = fmax6(u)
+print("최대값: ", max)
+
+print()
+print('------------------------------')
+
+def fmax7(fu):
+    
+    max = fu[0]    
+
+    for i in range(1, len(fu)):
+        if max < fu[i]:
+            max = fu[i]
+    
+    return max
+
+v = [5, 4, 1, 7, 8, 12, 15, 3, 17]
+max = fmax7(v)
 print("최대값: ", max)
